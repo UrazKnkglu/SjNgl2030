18f703b081c0ca92910024705f17040b249ece92
Alp Göncü <79418811+Alpgonjustvi@users.noreply.github.com>
2025-07-28 19:39:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,18 +4,16 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-// DB bağlantısı
 connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public")); // register gibi dosyaları sunar
+app.use(express.static("public"));
 
-app.use("/api", require("./routes/auth")); // Register route burada
+app.use("/api", require("./routes/auth"));
 
 if (!global.tokenBlacklist) global.tokenBlacklist = new Set();
 
-// Dinleme
 app.listen(1001, "0.0.0.0", () => {
     console.log("Server is running at http://192.168.1.5:1001");
 });
